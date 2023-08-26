610fde6686e9e144456f87e0b6efd927abfdbaed
Breno Dantas <brenodantas086@gmail.com>
2023-08-26 19:07:15 -0300

    feat: Melhoria do frontend

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from app import app
+from app import database
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+@app.route("/clientes")
+def listar_clientes():
+    clientes = database.Cliente.query.all()
+    return render_template("clientes.html", clientes=clientes)
